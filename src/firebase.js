b70c893d499eaffbc4f1efcd6750eb20f8417865
omer-fsdev <119896990+omer-fsdev@users.noreply.github.com>
2024-11-08 18:11:49 +0100

    feat(user-auth): add login functionality

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfTUWX3FYKa9NnCdm-LIX5B5xCrmeG9fM",
+  authDomain: "cinescope-web-app.firebaseapp.com",
+  projectId: "cinescope-web-app",
+  storageBucket: "cinescope-web-app.firebasestorage.app",
+  messagingSenderId: "26849801396",
+  appId: "1:26849801396:web:68756262ace26ff88613e1",
+};
+
+const app = initializeApp(firebaseConfig);
