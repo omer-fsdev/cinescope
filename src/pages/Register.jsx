44a2bab644a4dcf8c46bda7245fae07f1fd59545
omer-fsdev <119896990+omer-fsdev@users.noreply.github.com>
2024-11-21 15:01:09 +0100

    style: Use tailwind classes for initial style and remove  some old codes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,20 +11,43 @@ const Register = () => {
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
-          <div>
-            <input name="floating_text" type="text" required />
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              name="floating_text"
+              type="text"
+              class="peer"
+              placeholder=" "
+              required
+            />
             <label htmlFor="floating_text">First Name</label>
           </div>
-          <div>
-            <input name="floating_text" type="text" required />
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              name="floating_text"
+              type="text"
+              class="peer"
+              placeholder=" "
+              required
+            />
             <label htmlFor="floating_text">Last Name</label>
           </div>
-          <div>
-            <input name="floating_email" type="email" />
-            <label htmlFor="floating_email">Email</label>
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              type="email"
+              name="floating_email"
+              class="peer"
+              placeholder=" "
+              required
+            />
+            <label for="floating_email">Email address</label>
           </div>
-          <div>
-            <input name="floating_password" type="password" />
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              name="floating_password"
+              type="password"
+              class="peer"
+              placeholder=" "
+            />
             <label htmlFor="floating_password">Password</label>
           </div>
           <button type="submit">Register</button>
