5a61ce8c95d1bb852591027f42e906faefd81a41
omer-fsdev <119896990+omer-fsdev@users.noreply.github.com>
2024-11-23 18:44:29 +0100

    refactor: Use tailwind classes for better readibility

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,10 +4,8 @@ import GoogleIcon from "../assets/icons/GoogleIcon";
 const Register = () => {
   return (
     <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
-      <div
-        className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
-      >
-        <form className="absolute inset-[2px] rounded-[8px] bg-gray-100 dark:bg-[#28292d] z-[10] flex flex-col py-[50px] px-[40px]">
+      <div className={`form-container mt-[5vh] w-[380px] h-[580px]`}>
+        <form>
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
@@ -50,8 +48,13 @@ const Register = () => {
             />
             <label htmlFor="floating_password">Password</label>
           </div>
-          <button type="submit">Register</button>
-          <button type="button">
+          <button className="btn-danger" type="submit">
+            Register
+          </button>
+          <button
+            className="btn-danger flex justify-between item-center"
+            type="button"
+          >
             Continue with Google
             <GoogleIcon color="currentColor" />
           </button>
