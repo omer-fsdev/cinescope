4b5be0d35713a7cb693e70e20775178dcfd20bfa
omer-fsdev <119896990+omer-fsdev@users.noreply.github.com>
2024-11-27 19:16:05 +0100

    feat: Return pages via AppRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Register from "./pages/Register";
+import AppRouter from "./router/AppRouter";
 const App = () => {
   return (
     <div>
-      <Register />
+      <AppRouter />
     </div>
   );
 };
