b3bd038f9779d9cb601862098261f587db94bdd4
omer-fsdev <119896990+omer-fsdev@users.noreply.github.com>
2024-11-30 16:00:43 +0100

    feat: Add value to Context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,7 +2,7 @@ import React, { createContext } from "react";
 
 export const AuthContext = createContext();
 const AuthContextProvider = ({ children }) => {
-  const values = {};
+  const values = { currentUser: { displayName: "WIP" } };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
