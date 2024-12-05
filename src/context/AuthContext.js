c72b8698bcfada55fbc5bd54b6c3d8542b76a0c7
omer-fsdev <119896990+omer-fsdev@users.noreply.github.com>
2024-12-05 18:32:35 +0100

    feat: Create func to create a user

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,16 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { createContext } from "react";
+import { auth } from "../auth/firebase";
 
 export const AuthContext = createContext();
+
+const createUser = async (email, password) => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error(error);
+  }
+};
 const AuthContextProvider = ({ children }) => {
   const values = { currentUser: { displayName: "WIP" } };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
